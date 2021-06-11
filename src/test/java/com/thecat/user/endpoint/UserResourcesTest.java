67a2816107e5c7774ee1d2c769c333f65edc702a
Felix Roberge <roberge.felix@gmail.com>
2021-06-11 16:29:03 -0400

    remove unecessary import

--- src/test/java/com/thecat/user/endpoint/UserResourcesTest.java
+++ src/test/java/com/thecat/user/endpoint/UserResourcesTest.java
@@ -7,8 +7,6 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
-import com.thecat.user.model.User;
-
 @QuarkusTest
 public class UserResourcesTest {
 
